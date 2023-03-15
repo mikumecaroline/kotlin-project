172e2bee77ca52dae11f196802779681a2860b11
eMobilis <ryankedi305@gmail.com>
2023-03-15 12:27:39 +0300

    simple numbering

--- src/main/kotlin/Compare execise.kt
+++ src/main/kotlin/Compare execise.kt
@@ -2,7 +2,7 @@ fun main(args: Array<String>) {
     print("Enter You Age")
     var age:Int = readLine()!!.toInt()
     if (age >= 18){
-        print("Welacome to the club")
+        print("Welcome to the club")
     }
     else{
         print("Sorry we cant admit underage")
---
+++ src/main/kotlin/Compare exercise _two.kt
@@ -0,0 +1,61 @@
+import javax.xml.crypto.Data
+
+fun main(args: Array<String>) {
+    print("Enter number")
+    var number:Int = readLine()!!.toInt()
+
+    if (number==0){
+        print("Sh20=1GB for 1hr")
+    }
+    if (number==1){
+        print("Data Deals")
+    }
+    if (number==2){
+        print("Daily Bundles")
+    }
+    if (number==3){
+        print("Weekly Bundles")
+    }
+    if (number==4){
+        print("GO MONTHLY")
+    }
+    if (number==5){
+        print("No Expiry")
+    }
+    if (number==6){
+        print("Video Bundles")
+    }
+    if (number==7){
+        print("Okoa Data")
+    }
+    if (number==8){
+      print("Lipa Mdogo")
+    }
+    if (number==9){
+        print("Data Plus New")
+    }
+    if (number==10){
+        print("Hot Minutes")
+    }
+    if (number==11){
+        print("Invalid Number")
+    }
+    else
+        if (number==98){
+            print("MORE")
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
