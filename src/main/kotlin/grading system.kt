be8303d4fbf439f5a98c1545a5eddf69b4d902b7
eMobilis <ryankedi305@gmail.com>
2023-03-15 12:48:58 +0300

    grading work

---
+++ src/main/kotlin/grading system.kt
@@ -0,0 +1,41 @@
+fun main(args: Array<String>) {
+    print("Enter grade scored")
+    var marks:Int= readLine()!!.toInt()
+
+    if (marks==90-100){
+        print("A Has a present")
+    }
+    else if (marks==80-89){
+        print("A- Congratulations")
+    }
+    else if (marks==70){
+        print("B+ good job")
+    }
+    else if (marks==60){
+    print("B Well done")
+    }
+    else if (marks==50){
+        print("B- Nice effort ")
+    }
+    else if (marks==40){
+        print("C+ Put more effort")
+    }
+    else
+        if (marks==39){
+        print("C and below to repeat the exams")
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
