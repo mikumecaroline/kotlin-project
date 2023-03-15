e29cd7b1773b916883af0324a3b03b7af03b8f48
eMobilis <ryankedi305@gmail.com>
2023-03-15 11:41:49 +0300

    data types in kotlin

---
+++ src/main/kotlin/data types.kt
@@ -0,0 +1,17 @@
+import java.io.ObjectInputFilter.Status
+
+fun main(args: Array<String>) {
+    //Integer or Int
+    var age:Int=20
+    //Strings
+    var name :String= "Hans"
+    //Double
+    var temperature:Double=36.51
+    //Boolean true or false
+    var Status=false
+    //Float
+    var point:Float=8.5F
+    //Convert double to Float
+    var marks = 46.587F
+
+}
